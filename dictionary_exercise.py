2716336de9b47460e1787e6cbaf2c747821deae0
Jake Morelli <jake.morelli98@gmail.com>
2022-02-07 13:12:35 -0600

    dictionary work

---
+++ dictionary_exercise.py
@@ -0,0 +1,48 @@
+Course_room = {
+    "Course Number": "Room Number",
+    "CS101": "3004",
+    "CS102": "4501",
+    "CS201": "6755",
+    "NT110": "1244",
+    "CM241": "1411",
+}
+
+print(Course_room)
+
+Course_instrucor = {
+    "Course Number": "Instructor",
+    "CS101": "Haynes",
+    "CS102": "Alvarado",
+    "CS201": "Rich",
+    "NT110": "Burke",
+    "CM241": "Lee",
+}
+
+print(Course_instrucor)
+
+Course_time = {
+    "Course Number": "Meeting Time",
+    "CS101": "8:00 a.m.",
+    "CS102": "9:00 a.m.",
+    "CS201": "10:00 a.m.",
+    "NT110": "11:00 a.m.",
+    "CM241": "1:00 p.m.",
+}
+
+print(Course_time)
+
+"""
+
+print()
+print("*****  start section 2 - search dictionary ********")
+print()
+
+name = "Chris"
+
+if name in phonebook:
+    print(phonebook[name])
+else:
+    print(name, "is not in the phonebook")
+
+
+"""
